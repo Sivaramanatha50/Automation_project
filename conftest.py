f31083a434746de4f16cba1a02da73865a657523
sivaram031 <sivaram031.md@gmail.com>
2025-01-07 23:35:47 +0530

    chrome version updated

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 @pytest.fixture
 def set_up_browser():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(driver_version="131.0.6778.205").install()))
     driver.maximize_window()
     yield driver
     driver.quit()
