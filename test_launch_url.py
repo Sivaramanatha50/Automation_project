212c92fb3c455433c78769a9d1ee40c8e8949d9a
sivaram031 <sivaram031.md@gmail.com>
2025-01-07 23:23:40 +0530

    changed import

--- test_launch_url.py
+++ test_launch_url.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 import pytest
-from test_data import url
+from data_file import url
 
 
 
